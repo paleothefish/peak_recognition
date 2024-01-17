12bbc549417f92af366cc15bf2338cfa6a367bfb
Allison Kuan <akuan@ucdavis.edu>
2024-01-17 13:30:50 -0800

    readjust resolution of plots to 256 x 256 temporarily to see peaks better

--- plot_chromatogram.py
+++ plot_chromatogram.py
@@ -32,7 +32,7 @@ class EICPlotter:
                 retention_index = np.linspace(min_ri, max_ri, len(intensities))
 
                 #Create a plot
-                fig, ax = plt.subplots(figsize=(1.28, 1.28))
+                fig, ax = plt.subplots(figsize=(2.56, 2.56))
                 #fig, ax = plt.subplots(figsize=(self.width, self.height))
                 #Plot the retention index and intensities
                 ax.plot(retention_index, intensities, linewidth=2.0, color='black')
@@ -68,7 +68,7 @@ class EICPlotter:
 # Usage of the EICPlotter class
 dir_path = r'D:\UCD_Fiehn_Lab\peaks_from_LCB'
 #Create an instance of the EICPlotter class
-plotter = EICPlotter(dir_path, 1, 1)
+plotter = EICPlotter(dir_path, 2, 2)
 #Call the plot method
 plotter.plot()
 
